7290b3547afd3141c38ad01d74f6e3a4b327e26f
Michael Seemann <michael.seemann@synaos.com>
2022-12-24 10:22:53 +0100

    Initial commit

---
+++ apps/app/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'reusable-mat-forms-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'app';
+}
